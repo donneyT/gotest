5959aa5f121a0d66c243808aeb5c33c3fcd0114f
donney <donney@mengyadeiMac.local>
2020-06-23 16:53:33 +0800

    first commit

---
+++ src/gce/gnet/request.go
@@ -0,0 +1,16 @@
+package gnet
+
+import "gce/giface"
+
+type Request struct {
+	conn giface.IConnection //已经和客户端建立好的 链接
+	data []byte //客户端请求的数据
+}
+//获取请求连接信息
+func(r *Request) GetConnection() giface.IConnection {
+	return r.conn
+}
+//获取请求消息的数据
+func(r *Request) GetData() []byte {
+	return r.data
+}
